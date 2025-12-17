b7e00a229607aa94ef912dc669e19117d7143574
Sophia <sophiaperez040922@gmail.com>
2025-12-17 14:54:10 -0700

    feat: adding gas calculations

--- basics.py
+++ basics.py
@@ -1,5 +1,8 @@
+import constants
+
 number_gas = 0
 number_oil = 0
+gst = 0
 
 print("***Welcome to Gas Station Program!***")
 print("Please select the type of purchase:")
@@ -16,8 +19,29 @@ if option == "g":
   if number_gas < 0:
     print("Number of gas liters shoulds be > 0")
     exit()
+
+  province_gas = input("Please enter the 2 letters province abbreviation: ").upper()
+  before_discount = int(constants.GAS_LITRE*number_gas)
+
+  if number_gas > 4000:
+    price_per_litre = constants.GAS_LITRE - (constants.GAS_LITRE*.10)
   else:
-    province_gas = input("Please enter the 2 letters province abbreviation: ")
+    price_per_litre = constants.GAS_LITRE
+
+  match province_gas:
+    case "AB" | "BC" | "MB" | "NT" | "NU" | "QC" | "SK" | "YT":
+      gst = constants.GST_ONE/100
+    case "ON":
+      gst = constants.GST_TWO/100
+    case _:
+      gst = constants.GST_THREE/100
+
+  after_discount = int(price_per_litre*number_gas)
+  gst = gst*after_discount
+  total = after_discount + gst
+
+  print(f"{'Product':<10} {'# of Litres':^15} {'Price Before Discount':^22} {'Price After Discount':^22} {'GST':^15} {'Total Price':^15}")
+  print(f"{'Gas':<10} {number_gas:^15.1f} {before_discount:^22.1f} {after_discount:^22.1f} {gst:^15.2f} {total:^15.2f}")
 
 if option == "o":
   number_oil = int(input("Enter the number of cases of Oil: "))
@@ -25,4 +49,5 @@ if option == "o":
     print("Number of oil cases should be > 0")
     exit()
   else:
-    province_oil = input("Please enter the 2 letters province abbreviation: ")
+    province_oil = input("Please enter the 2 letters province abbreviation: ").upper()
+
